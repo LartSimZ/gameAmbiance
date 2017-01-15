f0c76037bce594be507842eef0451f731fb3a273
LartSimZ <lartsimz@gmail.com>
2017-01-15 23:13:27 +0100

    Updated SSD1306 driver class with helpers related todata sending

--- hw/display_driver_ssd1306.cpp
+++ hw/display_driver_ssd1306.cpp
@@ -4,6 +4,26 @@ namespace gameAmbiance
 {
     namespace hw
     {
+        void display_driver_ssd1306::sendCommand(uint8_t c) const
+        {
+            _busDriver.sendCommand(_dcPin, c);
+        }
+
+        void display_driver_ssd1306::sendCommand(uint8_t c0, uint8_t c1) const
+		{
+            _busDriver.sendCommand(_dcPin, c0, c1);
+		}
+
+		void display_driver_ssd1306::sendCommand(uint8_t c0, uint8_t c1, uint8_t c2) const
+		{
+            _busDriver.sendCommand(_dcPin, c0, c1, c2);
+		}
+
+		void display_driver_ssd1306::sendData(uint8_t* buf, uint32_t len) const
+		{
+            _busDriver.sendCommand(_dcPin, buf, len);
+		}
+
         display_driver_ssd1306::display_driver_ssd1306(const bus_driver_interface& busDriver, uint8_t dcPin, uint8_t rstPin, int16_t width, int16_t height)
             : _busDriver(busDriver)
             , _dcPin(dcPin)
@@ -24,22 +44,22 @@ namespace gameAmbiance
 
         void display_driver_ssd1306::init()
         {
-            _busDriver.sendCommand(SSD_Display_Off);                    // 0xAE
-            _busDriver.sendCommand(SSD1306_SETDISPLAYCLOCKDIV, 0x80);      // 0xD5 + the suggested ratio 0x80
-            _busDriver.sendCommand(SSD1306_SETMULTIPLEX, 0x3F);
-            _busDriver.sendCommand(SSD1306_SETDISPLAYOFFSET, 0x00);        // 0xD3 + no offset
-            _busDriver.sendCommand(SSD1306_SETSTARTLINE | 0x0);            // line #0
-            _busDriver.sendCommand(SSD1306_CHARGEPUMP, 0x14);
-            _busDriver.sendCommand(SSD1306_MEMORYMODE, 0x00);              // 0x20 0x0 act like ks0108
-            _busDriver.sendCommand(SSD1306_SEGREMAP | 0x1);
-            _busDriver.sendCommand(SSD1306_COMSCANDEC);
-            _busDriver.sendCommand(SSD1306_SETCOMPINS, 0x12);  // 0xDA
-            _busDriver.sendCommand(SSD_Set_ContrastLevel, 0xCF);
-            _busDriver.sendCommand(SSD1306_SETPRECHARGE, 0x22); // 0xd9
-            _busDriver.sendCommand(SSD1306_SETVCOMDETECT, 0x40);  // 0xDB
-            _busDriver.sendCommand(SSD1306_DISPLAYALLON_RESUME);    // 0xA4
-            _busDriver.sendCommand(SSD1306_Normal_Display);         // 0xA6
-            _busDriver.sendCommand(SSD_Display_On);
+            sendCommand(SSD_Display_Off);                    // 0xAE
+            sendCommand(SSD1306_SETDISPLAYCLOCKDIV, 0x80);      // 0xD5 + the suggested ratio 0x80
+            sendCommand(SSD1306_SETMULTIPLEX, 0x3F);
+            sendCommand(SSD1306_SETDISPLAYOFFSET, 0x00);        // 0xD3 + no offset
+            sendCommand(SSD1306_SETSTARTLINE | 0x0);            // line #0
+            sendCommand(SSD1306_CHARGEPUMP, 0x14);
+            sendCommand(SSD1306_MEMORYMODE, 0x00);              // 0x20 0x0 act like ks0108
+            sendCommand(SSD1306_SEGREMAP | 0x1);
+            sendCommand(SSD1306_COMSCANDEC);
+            sendCommand(SSD1306_SETCOMPINS, 0x12);  // 0xDA
+            sendCommand(SSD_Set_ContrastLevel, 0xCF);
+            sendCommand(SSD1306_SETPRECHARGE, 0x22); // 0xd9
+            sendCommand(SSD1306_SETVCOMDETECT, 0x40);  // 0xDB
+            sendCommand(SSD1306_DISPLAYALLON_RESUME);    // 0xA4
+            sendCommand(SSD1306_Normal_Display);         // 0xA6
+            sendCommand(SSD_Display_On);
         }
 
         void display_driver_ssd1306::term()
--- hw/display_driver_ssd1306.h
+++ hw/display_driver_ssd1306.h
@@ -74,6 +74,11 @@ namespace gameAmbiance
             int16_t _screenHeight;
             uint8_t* _screenBuffer;
 
+			void sendCommand(uint8_t c) const;
+        	void sendCommand(uint8_t c0, uint8_t c1) const;
+			void sendCommand(uint8_t c0, uint8_t c1, uint8_t c2) const;
+			void sendData(uint8_t* buf, uint32_t len) const;
+
 		public:
 			display_driver_ssd1306(const bus_driver_interface& busDriver, uint8_t dcPin, uint8_t rstPin, int16_t width, int16_t height);
 			~display_driver_ssd1306();
